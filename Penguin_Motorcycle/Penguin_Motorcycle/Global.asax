9d6c4f0bb04604e77752cb55a1edfddb6cfec4c4
unknown <mingqxu@mqxulaptop-x1.redmond.corp.microsoft.com>
2015-02-23 11:38:29 +0800

    create the site

---
+++ Penguin_Motorcycle/Penguin_Motorcycle/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Penguin_Motorcycle.MvcApplication" Language="C#" %>
---
+++ Penguin_Motorcycle/Penguin_Motorcycle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Penguin_Motorcycle.Startup))]
+namespace Penguin_Motorcycle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
